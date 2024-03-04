43d496905b37706b3af2dc112da312b64963b3e9
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-03-04 20:29:09 +0100

    implement ringbuffer and add tests

---
+++ tests/lib/myLib/src/RingBuffertTests.cpp
@@ -0,0 +1,148 @@
+#include <myLib/RingBuffer.hpp>
+
+#include <zephyr/ztest.h>
+
+ZTEST_SUITE(ringBuffer_tests, NULL, NULL, NULL, NULL, NULL);
+
+ZTEST(ringBuffer_tests, test_index_prefix_increment_decrement)
+{
+    myLib::internal::RingIndex<4> idx;
+
+    zassert_equal(idx, 0);
+    zassert_equal(++idx, 1);
+    zassert_equal(++idx, 2);
+
+    zassert_equal(idx, 2);
+    zassert_equal(--idx, 1);
+    zassert_equal(--idx, 0);
+}
+
+ZTEST(ringBuffer_tests, test_index_postfix_increment_decrement)
+{
+    myLib::internal::RingIndex<4> idx;
+
+    zassert_equal(idx++, 0);
+    zassert_equal(idx++, 1);
+    zassert_equal(idx, 2);
+
+    zassert_equal(idx--, 2);
+    zassert_equal(idx--, 1);
+    zassert_equal(idx, 0);
+}
+
+ZTEST(ringBuffer_tests, test_index_increment_wraparound)
+{
+    static constexpr std::size_t size = 3;
+    myLib::internal::RingIndex<size> idx;
+
+    for (int i = 0; i < size; ++i) {
+        ++idx;
+    }
+    zassert_equal(idx, size - 1);
+    ++idx;
+    zassert_equal(idx, 0);
+}
+
+ZTEST(ringBuffer_tests, test_index_increment_wraparound_powerOf2)    // TODO ??
+{
+    static constexpr std::size_t size = 8;
+    myLib::internal::RingIndex<size> idx;
+
+    for (int i = 0; i < size; ++i) {
+        ++idx;
+    }
+    zassert_equal(idx, size - 1);
+    ++idx;
+    zassert_equal(idx, 0);
+}
+
+ZTEST(ringBuffer_tests, test_index_decrement_wraparound)
+{
+    static constexpr std::size_t size = 8;
+    myLib::internal::RingIndex<size> idx;
+
+    --idx;
+    zassert_equal(idx, size - 1);
+}
+
+ZTEST(ringBuffer_tests, test_buffer_empty)
+{
+    myLib::RingBuffer<int, 8> buf;
+
+    zassert_true(buf.isEmpty());
+
+    buf.push(42);
+    zassert_false(buf.isEmpty());
+
+    buf.pull();
+    zassert_true(buf.isEmpty());
+}
+
+ZTEST(ringBuffer_tests, test_buffer_full)
+{
+    myLib::RingBuffer<int, 2> buf;
+
+    zassert_false(buf.isFull());
+
+    buf.push(42);
+    zassert_false(buf.isFull());
+
+    buf.push(42);
+    zassert_true(buf.isFull());
+
+    buf.pull();
+    zassert_false(buf.isFull());
+}
+
+ZTEST(ringBuffer_tests, test_push_and_pull)
+{
+    myLib::RingBuffer<int, 8> buf;
+
+    zassert_true(buf.push(42));
+
+    zassert_equal(buf.pull(), 42);
+}
+
+ZTEST(ringBuffer_tests, test_items_order)
+{
+    const std::array<int, 4> expected = {1, 2, 3, 4};
+
+    myLib::RingBuffer<int, 4> buf;
+
+    for (int v : expected) {
+        buf.push(v);
+    }
+
+    std::array<int, 4> actual;
+    for (int& v : actual) {
+        v = buf.pull();
+    }
+
+    zassert_mem_equal(expected.data(), actual.data(), expected.size());
+}
+
+static constexpr int threadPrio = 0;
+static constexpr int stackSize = 1024;
+
+// void producerTask()
+
+// K_THREAD_DEFINE(producer, stackSize, producerTask, NULL, NULL, NULL, threadPrio, 0, 0);
+// K_THREAD_DEFINE(consumer, stackSize, consumerTask, NULL, NULL, NULL, threadPrio, 0, 0);
+
+// ZTEST(ringBuffer_tests, test_thread_safety)
+// {
+//     const std::array<int, 4> expected = {1, 2, 3, 4};
+
+//     myLib::RingBuffer<int, 4> buf;
+
+//     for (int v : expected) {
+//         buf.push(v);
+//     }
+
+//     std::array<int, 4> actual;
+//     for (int& v : actual) {
+//         v = buf.pull();
+//     }
+
+//     zassert_mem_equal(expected.data(), actual.data(), expected.size());
+// }
