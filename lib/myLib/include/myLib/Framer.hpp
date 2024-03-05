c4f36123fb27a060d3cc96292a24576c89a4b9dd
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-03-05 22:13:04 +0100

    create framer

---
+++ lib/myLib/include/myLib/Framer.hpp
@@ -0,0 +1,41 @@
+#ifndef LIB_MYLIB_INCLUDE_MYLIB_FRAMER_H
+#define LIB_MYLIB_INCLUDE_MYLIB_FRAMER_H
+
+#include <cstdint>
+
+template <typename BUFFER_T>
+class Framer {
+  public:
+    constexpr Framer(BUFFER_T& buf) noexcept
+     : _buf(buf)
+    { }
+
+    void deframe(std::uint8_t b) noexcept
+    {
+        switch (_state) {
+            case State::Header:
+                _len = b;
+                _state = State::Data;
+                break;
+
+            case State::Data:
+                if (_len-- <= 0) {
+                    return;    // TODO error?
+                }
+
+                _buf.push(b);
+                break;
+        }
+    }
+
+    constexpr bool msgComplete() const noexcept { return _len <= 0; }
+
+  private:
+    enum class State { Header, Data };
+
+    BUFFER_T& _buf;
+    State _state = State::Header;
+    std::size_t _len{};
+};
+
+#endif    // LIB_MYLIB_INCLUDE_MYLIB_FRAMER_H
