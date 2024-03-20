502d3e74fe7745e61f09c0a615bb654a786f079c
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-03-20 20:21:35 +0100

    improve sensorservice, add expected

--- app/include/SensorService.hpp
+++ app/include/SensorService.hpp
@@ -5,68 +5,36 @@
 #include <zephyr/drivers/sensor.h>
 #include <zephyr/kernel.h>
 
+#include <expected>
 #include <array>
 #include <cstdint>
 
-K_THREAD_STACK_DEFINE(threadStack, CONFIG_SENSORSERVICE_STACKSIZE);
+enum class SensorError {
+    InvalidId,
+    NotPresent,
+    ReadFail,
+};
 
 class SensorService {
   public:
     constexpr SensorService() noexcept { }
 
-    void init() noexcept
-    {
-        for (const auto sensor : _tempSensors) {
-            if (!device_is_ready(sensor)) {
-                printk("Sensor not ready\n");    // TODO replace with logs
-                return;
-            }
-        }
-
-        _threadId = k_thread_create(&_thread, threadStack, K_THREAD_STACK_SIZEOF(threadStack), threadEntry, this,
-                                    nullptr, nullptr, prio, 0, K_FOREVER);
-    }
-
-    void start() const noexcept { k_thread_start(_threadId); }
-
+    bool init() const noexcept;
+    void start() noexcept;
     void stop() const noexcept { k_thread_abort(_threadId); }
-
-    int get(std::size_t idx) noexcept
-    {
-        if (idx >= _tempSensors.size()) {
-            return 0;    // TODO return error code
-        }
-
-        struct sensor_value temp;
-        sensor_channel_get(_tempSensors[idx], SENSOR_CHAN_AMBIENT_TEMP, &temp);
-        return sensor_value_to_milli(&temp);
-    }
+    auto get(std::size_t idx) const noexcept -> std::expected<int, SensorError>;
 
   private:
-    static constexpr std::array<const device* const, 3> _tempSensors{
+    static constexpr std::array<const device* const, 3> tempSensors{
         DEVICE_DT_GET(DT_NODELABEL(sens0)),
         DEVICE_DT_GET(DT_NODELABEL(sens1)),
         DEVICE_DT_GET(DT_NODELABEL(sens2)),
     };
 
-    static constexpr int prio = 0;
-
-    void fetchSensors()
-    {
-        while (1) {
-
-            for (const auto sensor : _tempSensors) {
-                sensor_sample_fetch(sensor);
-            }
-            printk("fetched sensors\n");
-
-            k_sleep(K_MSEC(CONFIG_SENSORSERVICE_FETCH_INTERVAL));
-        }
-    }
-
+    void fetchSensors() const noexcept;
     static void threadEntry(void* p1, void*, void*) noexcept { reinterpret_cast<SensorService*>(p1)->fetchSensors(); }
 
-    k_thread _thread;
+    k_thread _thread{};
     k_tid_t _threadId;
 };
 
---
+++ app/src/SensorService.cpp
@@ -0,0 +1,62 @@
+#include "SensorService.hpp"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_REGISTER(sensorService, CONFIG_SENSORSERVICE_LOG_LEVEL);
+
+namespace {
+
+K_THREAD_STACK_DEFINE(threadStack, CONFIG_SENSORSERVICE_STACKSIZE);
+
+}
+
+bool SensorService::init() const noexcept
+{
+    for (const auto sensor : tempSensors) {
+        if (!device_is_ready(sensor)) {
+            LOG_WRN("Sensor not ready\n");
+            return false;
+        }
+    }
+    return true;
+}
+
+void SensorService::start() noexcept
+{
+    _threadId = k_thread_create(&_thread, threadStack, K_THREAD_STACK_SIZEOF(threadStack), threadEntry, this, nullptr,
+                                nullptr, CONFIG_SENSORSERVICE_THREAD_PRIO, 0, K_NO_WAIT);
+}
+
+auto SensorService::get(std::size_t idx) const noexcept -> std::expected<int, SensorError>
+{
+    if (idx >= tempSensors.size()) {
+        return std::unexpected(SensorError::InvalidId);
+    }
+
+    struct sensor_value temp;
+    if (const int rc = sensor_channel_get(tempSensors[idx], SENSOR_CHAN_AMBIENT_TEMP, &temp); rc != 0) {
+        return std::unexpected(SensorError::ReadFail);
+    }
+
+    const int val = sensor_value_to_milli(&temp);
+
+    if (val == -25000) {    // TODO arbitrary number, calculate somehow
+        return std::unexpected(SensorError::NotPresent);
+    }
+
+    return val;
+}
+
+void SensorService::fetchSensors() const noexcept
+{
+    while (1) {
+
+        for (const auto sensor : tempSensors) {
+            if (const int rc = sensor_sample_fetch(sensor); rc != 0) {
+                LOG_ERR("failed to fetch sensor, %d", rc);
+            }
+        }
+
+        k_sleep(K_MSEC(CONFIG_SENSORSERVICE_FETCH_INTERVAL));
+    }
+}
