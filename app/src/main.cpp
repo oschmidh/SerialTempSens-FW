4752b04bc054d9603777d275dd487203210bec17
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-03-24 10:36:05 +0100

    use new uartAbs in main

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,60 +2,21 @@
 
 #include "WriteBuffer.hpp"
 #include "SensorService.hpp"
+#include "UartReceiveBuffer.hpp"
 
+#include <myLib/Uart.hpp>
 #include <ReadBufferFixedSize.h>
 
 #include <zephyr/device.h>
-#include <zephyr/drivers/uart.h>
 #include <zephyr/drivers/sensor.h>
-#include <zephyr/usb/usb_device.h>
+// #include <zephyr/usb/usb_device.h>
 #include <zephyr/logging/log.h>
 
 LOG_MODULE_REGISTER(main, CONFIG_APP_LOG_LEVEL);
 
 using ReadBufferType = EmbeddedProto::ReadBufferFixedSize<32>;
 
-K_MSGQ_DEFINE(uart_msgq, sizeof(ReadBufferType), 10, 1);
-
 static const struct device* const uart_dev = DEVICE_DT_GET(DT_CHOSEN(zephyr_com_uart));
-
-void serial_cb(const struct device* dev, void* user_data)
-{
-    enum class State { Header, Data };
-
-    uint8_t c;
-    static ReadBufferType rx_buf;
-    static State state;
-    static int n_bytes = 0;
-
-    if (!uart_irq_update(uart_dev)) {
-        return;
-    }
-
-    if (!uart_irq_rx_ready(uart_dev)) {
-        return;
-    }
-
-    while (uart_fifo_read(uart_dev, &c, 1) == 1) {
-
-        switch (state) {
-            case State::Header:
-
-                n_bytes = c;
-                state = State::Data;
-                break;
-
-            case State::Data:
-                rx_buf.push(c);
-                if (--n_bytes <= 0) {
-                    k_msgq_put(&uart_msgq, &rx_buf, K_NO_WAIT);
-                    rx_buf.clear();
-                    state = State::Header;
-                }
-                break;
-        }
-    }
-}
 
 int main()
 {
@@ -69,13 +30,20 @@ int main()
         return 0;
     }
 
-    if (uart_irq_callback_user_data_set(uart_dev, serial_cb, NULL) != 0) {
-        return 0;
-    }
-    uart_irq_rx_enable(uart_dev);
+    UartReceiveBuffer<32> rxBuf{};
+    Uart uart{uart_dev, rxBuf};
 
-    ReadBufferType readBuf;
-    while (k_msgq_get(&uart_msgq, &readBuf, K_FOREVER) == 0) {
+    uart.init();
+
+    uart.enableRx();
+
+    while (1) {
+        const std::uint8_t numBytes = rxBuf.pull();
+
+        ReadBufferType readBuf;
+        while (readBuf.get_size() < numBytes) {
+            readBuf.push(rxBuf.pull());
+        }
 
         Command receivedCmd;
         if (receivedCmd.deserialize(readBuf) == ::EmbeddedProto::Error::NO_ERRORS) {
@@ -95,11 +63,8 @@ int main()
             if (rply.serialize(writeBuf) == ::EmbeddedProto::Error::NO_ERRORS) {
 
                 const int n_bytes = writeBuf.get_size();
-                uart_poll_out(uart_dev, n_bytes);
-
-                for (std::uint8_t b : writeBuf) {
-                    uart_poll_out(uart_dev, b);
-                }
+                uart.sendPolling(n_bytes);
+                uart.sendPolling(writeBuf);
             }
         }
     }
