dbf5544ad9ebb0aaf49e614bf224afbbc681fb13
oschmidh <74605541+oschmidh@users.noreply.github.com>
2024-03-24 14:18:28 +0100

    fix and add tests

--- tests/app/UartReceiveBuffer/src/UartReceiveBufferTests.cpp
+++ tests/app/UartReceiveBuffer/src/UartReceiveBufferTests.cpp
@@ -3,11 +3,13 @@
 #include <zephyr/kernel.h>
 #include <zephyr/ztest.h>
 
+#include <array>
+
 ZTEST_SUITE(uartReceiveBuffer_tests, NULL, NULL, NULL, NULL, NULL);
 
 ZTEST(uartReceiveBuffer_tests, test_push_pull)
 {
-    UartReceiveBuffer buf{};
+    UartReceiveBuffer<32> buf{};
 
     zassert_true(buf.push(5));
     const auto ret = buf.pull();
@@ -18,14 +20,15 @@ ZTEST(uartReceiveBuffer_tests, test_push_pull)
 
 ZTEST(uartReceiveBuffer_tests, test_push_pull_multiple)
 {
-    const std::uint8_t testData[] = {1, 2, 3, 4, 5};
-    UartReceiveBuffer buf{};
+    const std::array<std::uint8_t, 5> testData = {1, 2, 3, 4, 5};
+
+    UartReceiveBuffer<32> buf{};
 
     for (std::uint8_t d : testData) {
         zassert_true(buf.push(d));
     }
 
-    for (int i = 0; i < testData.size(); ++i) {
+    for (unsigned int i = 0; i < testData.size(); ++i) {
         const auto ret = buf.pull();
         zassert_true(ret.has_value());
         zassert_equal(ret.value(), testData[i]);
@@ -35,38 +38,31 @@ ZTEST(uartReceiveBuffer_tests, test_push_pull_multiple)
 static constexpr int threadPrio = 0;
 static constexpr int stackSize = 1024;
 
-void producerTask(void* buffer, void* inp, void*)
-{
-    UartReceiveBuffer* buf = reinterpret_cast<UartReceiveBuffer*>(buffer);
-    const std::uint8_t* inp = reinterpret_cast<std::uint8_t*>(inp);
-
-    zassert_true(buffer->push(*inp));
-}
-
 void consumerTask(void* buffer, void* outp, void*)
 {
-    UartReceiveBuffer* buf = reinterpret_cast<UartReceiveBuffer*>(buffer);
+    UartReceiveBuffer<32>* buf = reinterpret_cast<UartReceiveBuffer<32>*>(buffer);
 
     std::optional<std::uint8_t>* out = reinterpret_cast<std::optional<std::uint8_t>*>(outp);
-    *out = buffer->pull();
+    *out = buf->pull();
 }
 
-ZTEST(ringBuffer_tests, test_pull_blocking)
+K_THREAD_STACK_DEFINE(consumerStack, stackSize);
+
+ZTEST(uartReceiveBuffer_tests, test_pull_blocking)
 {
     const std::uint8_t input = 77;
-    UartReceiveBuffer buf{};
+    UartReceiveBuffer<32> buf{};
     std::optional<std::uint8_t> output;
 
-    K_THREAD_DEFINE(consumerThread, stackSize, consumerTask, reinterpret_cast<void*>(&buf),
-                    reinterpret_cast<void*>(&output), NULL, threadPrio, 0, 0);
+    k_thread consumerThread{};
+    k_thread_create(&consumerThread, consumerStack, K_THREAD_STACK_SIZEOF(consumerStack), consumerTask,
+                    reinterpret_cast<void*>(&buf), reinterpret_cast<void*>(&output), NULL, threadPrio, 0, K_NO_WAIT);
 
-    zassert_false(k_thread_join(consumerThread, K_MSEC(1000)));
+    zassert_equal(k_thread_join(&consumerThread, K_MSEC(1000)), -EAGAIN);
 
-    K_THREAD_DEFINE(producerThread, stackSize, producerTask, reinterpret_cast<void*>(&buf),
-                    reinterpret_cast<void*>(&input), NULL, threadPrio, 0, 0);
+    buf.push(input);
 
-    zassert_true(k_thread_join(producerThread, K_MSEC(1000)));
-    zassert_true(k_thread_join(consumerThread, K_MSEC(1000)));
+    zassert_equal(k_thread_join(&consumerThread, K_MSEC(1000)), 0);
 
     zassert_true(output.has_value());
     zassert_equal(output.value(), input);
